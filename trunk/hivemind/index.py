c847c00f5dcf22a2ef6762645220cf68f39d1e19
sopo.dev <sopo.dev@cba59794-df03-11dd-a8eb-ddf07f3c1838>
2009-01-30 06:23:20 +0000

    First HiveMind usable shot.

--- trunk/hivemind/index.py
+++ trunk/hivemind/index.py
@@ -1,4 +1,4 @@
-    #!/usr/bin python
+#!/usr/bin python
 # -*- coding: utf-8 -*-
 
 import os
@@ -7,10 +7,15 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from hivemind.hmdb import Planet
+from hivemind.hmdb import Fleet
+from hivemind.hivemind import Updater
 
 
-HAPI_BASE_URL = 'http://www.hyperiums.com/servlet/HAPI'
-ALLOWED_USERS = ('sopo')
+class Const:
+    HAPI_BASE_URL = 'http://www.hyperiums.com/servlet/HAPI'
+    HAPI_REQ_URL = ''
+    ALLOWED_USERS = ('sopo')
 
 
 class HAPIlogin(webapp.RequestHandler):
@@ -21,15 +26,14 @@ class HAPIlogin(webapp.RequestHandler):
     def post(self):
         login = cgi.escape(self.request.get('login')).lower()
         hapikey = cgi.escape(self.request.get('hapikey'))
-        if login in ALLOWED_USERS:
-            response = urlfetch.fetch(''.join([HAPI_BASE_URL,
+        if login in Const.ALLOWED_USERS:
+            response = urlfetch.fetch(''.join([Const.HAPI_BASE_URL,
                                                '?game=Hyperiums5',
                                                '&player=', login,
                                                '&hapikey=', hapikey]))
             if response.status_code == 200:
-                hapi_req_url = '&'.join(response.content.split('&')[0:-1])
-                print
-                print hapi_req_url
+                Const.HAPI_REQ_URL = '?'.join([Const.HAPI_BASE_URL,
+                                '&'.join(response.content.split('&')[0:-1])])
             path = os.path.join(os.path.dirname(__file__), 'index.html')
         else:
             path = os.path.join(os.path.dirname(__file__), 'error.html')
@@ -37,16 +41,43 @@ class HAPIlogin(webapp.RequestHandler):
 
 
 class Update(webapp.RequestHandler):
-    def get(self):
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'update.html')
+    def post(self):
+        response = urlfetch.fetch('&'.join([Const.HAPI_REQ_URL,
+                                            'request=getfleetsinfo',
+                                            'planet=*',
+                                            'data=foreign_planets']))
+        if response.status_code == 200:
+            database = Updater(response.content)
+            database.update()
+            status = 'Database successfully updated'
+        else:
+            status = 'Error while updating database'
+        template_values = {
+            'status': status
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
 
 class Search(webapp.RequestHandler):
-    def get(self):
-        template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'search.html')
+    def post(self):
+        searchby = self.request.get('searchby')
+        searched_term = cgi.escape(self.request.get('searched_term')).lower()
+        if searchby == 'player':
+            query = Fleet.gql("WHERE owner_name = :1 "
+                              "ORDER BY location_name",
+                              searched_term)
+        elif searchby == 'planet':
+            query = Fleet.gql("WHERE location_name = :1 "
+                              "ORDER BY owner_name",
+                              searched_term)
+        results = query.fetch(999)
+        template_values = {
+            'searched_term': searched_term,
+            'searchby': searchby,
+            'search_result': results
+        }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
 
